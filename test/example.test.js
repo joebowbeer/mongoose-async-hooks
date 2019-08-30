2147623c19fbb91dac373c3d9232cfeda5f3f412
Joe Bowbeer <joe.bowbeer@gmail.com>
2019-08-30 13:39:26 -0700

    Add new parser option, retain original test, drop pre 9.6 mode

--- test/example.test.js
+++ test/example.test.js
@@ -9,7 +9,7 @@ const { Schema } = mongoose;
 
 describe('Examples', function() {
   before(function() {
-    mongoose.connect('mongodb://localhost:27017/asynchooks');
+    mongoose.connect('mongodb://localhost:27017/asynchooks', { useNewUrlParser: true });
   });
 
   after(function() {
@@ -17,6 +17,43 @@ describe('Examples', function() {
   });
 
   describe('integrates with Node.js async hooks', function () {
+    const getAssertFn = (mode, myModelName) => {
+      return done => {
+        const types = [];
+        const hooks = {
+          init: (asyncId, type) => {
+            types.push(type);
+          }
+        };
+
+        const asyncHook = require('async_hooks').createHook(hooks);
+
+        const schema = new Schema({ name: String });
+
+        // Add this plugin
+        schema.plugin(mongooseAsyncHooks, { mode });
+
+        const MyModel = mongoose.model(myModelName, schema);
+
+        asyncHook.enable();
+
+        const doc = new MyModel({ name: 'test' });
+        doc.save(function(error, doc) {
+          asyncHook.disable();
+
+          assert.ok(types.includes('mongoose.' + myModelName));
+          // acquit:ignore:start
+          done();
+          // acquit:ignore:end
+        });
+      };
+    };
+
+    // node>=9.6.0
+    it('scope mode', getAssertFn('scope', 'MyModel'));
+  });
+
+  describe('integrates with Node.js async hooks without callback', function () {
     const getAssertFn = (mode, myModelName) => {
       return async () => {
         const types = [];
@@ -46,8 +83,5 @@ describe('Examples', function() {
 
     // node>=9.6.0
     it('scope mode', getAssertFn('scope', 'MyModel'));
-
-    // node<9.6.0
-    it('emit mode', getAssertFn('emit', 'MyAnotherModel'));
   });
 });
